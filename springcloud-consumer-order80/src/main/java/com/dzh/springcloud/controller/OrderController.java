bed1855abec42fb32ce654e0ade598a2c0018bfd
dzh <1060684139@qq.com>
2021-08-26 17:56:54 +0800

    消费者订单模块

---
+++ springcloud-consumer-order80/src/main/java/com/dzh/springcloud/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.dzh.springcloud.controller;
+
+import com.dzh.springcloud.entities.CommonResult;
+import com.dzh.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class, id);
+    }
+}
